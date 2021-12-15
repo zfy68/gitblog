15c302581745932d96ff96754cd068baeb866887
zfy <37278360+zfy68@users.noreply.github.com>
2021-12-15 22:29:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from feedgen.feed import FeedGenerator
 from lxml.etree import CDATA
 
 MD_HEAD = """## Gitblog
-My personal blog using issues and GitHub Actions (随意转载，无需署名)
+My personal blog using issues and GitHub Actions
 [RSS Feed](https://raw.githubusercontent.com/{repo_name}/master/feed.xml)
 """
 
